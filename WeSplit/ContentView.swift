7d30db2f31643188decfbac06fb97102e9584a1b
Artem <Serebryakov>
2022-06-01 18:49:19 +0300

    added slider

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPercentage  = 20
+    @State private var tipPercentage  = 20.0
     @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [0, 10, 15, 20, 25]
@@ -49,17 +49,31 @@ struct ContentView: View {
                     }
                 }
                 
-                Section {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self){
-                            Text($0, format: .percent)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                } header: {
+//                Section {
+//                    Picker("Tip percentage", selection: $tipPercentage) {
+//                        ForEach(tipPercentages, id: \.self){
+//                            Text($0, format: .percent)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                } header: {
+//                    Text("How much tip do you want to leave?")
+//                }
+                
+                VStack {
                     Text("How much tip do you want to leave?")
-                }
 
+                        Slider( value: $tipPercentage, in: 0...100, step: 5)
+                        {
+                        } minimumValueLabel: {
+                            Text("0")
+                        } maximumValueLabel: {
+                            Text("100")
+                        }
+//                    Text("\(Int(tipPercentage)) %")
+                    Text(tipPercentage, format: .percent)
+
+                }
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 } header: {
@@ -74,7 +88,7 @@ struct ContentView: View {
 
             }
             .navigationTitle("WeSplit")
-            .toolbar{
+            .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
                     Spacer()
                     
